4eab6858cf287881db959971c3b1c68f95d06da5
thibault.douilly <thibault.douilly@1bc5c555-484e-e99a-1e14-4397b19f8d34>
2013-02-20 12:53:57 +0000

    Ajout de la liste des couches (2)

--- trunk/Smart/src/fr/umlv/lastproject/smart/LayersActivity.java
+++ trunk/Smart/src/fr/umlv/lastproject/smart/LayersActivity.java
@@ -1,18 +1,43 @@
 package fr.umlv.lastproject.smart;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LayersActivity extends Activity {
+public class LayersActivity extends ListActivity {
+
+	private ListOverlay listOverlay = new ListOverlay();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_layers);
+
+		listOverlay = (ListOverlay) getIntent().getExtras().get("overlays");
+
+		setListAdapter(new ArrayAdapter<String>(this,
+				R.layout.listview_layers_items, listOverlay.toArray()));
+
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+
 	}
 
 	@Override
