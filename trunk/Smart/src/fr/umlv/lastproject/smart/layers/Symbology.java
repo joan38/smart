6241aea34e3e790974fc8f83c794c61fefcd6274
marcphilippe.barat <marcphilippe.barat@1bc5c555-484e-e99a-1e14-4397b19f8d34>
2013-02-21 14:52:38 +0000

    premiere version symbo

--- trunk/Smart/src/fr/umlv/lastproject/smart/layers/Symbology.java
+++ trunk/Smart/src/fr/umlv/lastproject/smart/layers/Symbology.java
@@ -1,13 +1,22 @@
 package fr.umlv.lastproject.smart.layers;
 
+import java.io.Serializable;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 /**
  * This class represent the geometry symbology
  * 
  * @author Fad's
  * 
  */
-public abstract class Symbology {
+public abstract class Symbology implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int color;
 
 	public Symbology(int color) {
@@ -15,10 +24,14 @@ public abstract class Symbology {
 	}
 
 	public void setColor(int color) {
-		this.color = color;
+		this.color = color;		
+		
 	}
 
 	public int getColor() {
+		
 		return this.color;
 	}
+	
+	public abstract Canvas getImage(final Bitmap bitmap);
 }
