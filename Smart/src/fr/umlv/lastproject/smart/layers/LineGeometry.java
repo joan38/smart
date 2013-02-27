9a82f2d3b78e153f021ae72cb78609217916c776
thibault <tbrun@etudiant.univ-mlv.fr>
2013-02-27 13:33:58 +0000

    Design pattern visiteurs pour les geometries

--- Smart/src/fr/umlv/lastproject/smart/layers/LineGeometry.java
+++ Smart/src/fr/umlv/lastproject/smart/layers/LineGeometry.java
@@ -3,6 +3,19 @@ package fr.umlv.lastproject.smart.layers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.osmdroid.views.MapView;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Region;
+import android.graphics.Paint.Style;
+import android.graphics.Path.FillType;
+import android.graphics.Rect;
+import fr.umlv.lastproject.smart.SmartMapView;
+import fr.umlv.lastproject.smart.form.SelectedGeometryListener;
+
 /**
  * This class represent the line geometry to draw
  * 
@@ -61,4 +74,52 @@ public class LineGeometry extends Geometry {
 		return this.points;
 	}
 
+	@Override
+	public void draw(MapView map, Canvas c, Boolean b, Symbology s) {
+
+		Paint paint = new Paint();
+		paint.setColor(s.getColor());
+		paint.setStyle(Style.FILL_AND_STROKE);
+		paint.setAlpha(150) ;
+
+		Path p = new Path() ;
+		
+		if (isSelected()) {
+			paint.setStrokeWidth(s.getSize() * 2);
+		} else {
+			paint.setStrokeWidth(s.getSize());
+		}
+
+		// Retrieving list of points contained
+
+
+		for (int j = 0; j < getPoints().size() ; j++) {
+			// on récupere les 2 points
+
+			// Converting coordinates in pixel
+			Point pixelA = map.getProjection().toPixels(getPoints().get(j% getPoints().size()).getCoordinates(),null);
+			if(j==0) p.moveTo(pixelA.x, pixelA.y);
+
+			p.lineTo(pixelA.x, pixelA.y);
+		}
+		p.close();
+		c.drawPath(p, paint);
+	}
+
+	@Override
+	public boolean isSelected(MapView m, Rect click) {
+
+		Path path = new Path() ;
+		Region clip = new Region(m.getProjection().toPixels(m.getBoundingBox())) ;
+		for(int i =0; i < getPoints().size() ; i++){
+			Point ps = m.getProjection().toPixels(getPoints().get(i).getCoordinates(),null);
+			if(i==0) path.moveTo(ps.x, ps.y);
+			else path.lineTo(ps.x, ps.y);							
+		}
+		clip.setPath(path, clip);
+		if(!clip.quickReject(click)){
+			return true ;
+		}
+		return false;
+	}
 }
--- Smart/src/fr/umlv/lastproject/smart/layers/PointGeometry.java
+++ Smart/src/fr/umlv/lastproject/smart/layers/PointGeometry.java
@@ -1,6 +1,15 @@
 package fr.umlv.lastproject.smart.layers;
 
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import fr.umlv.lastproject.smart.form.SelectedGeometryListener;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 
 /**
  * This class represent the Point geometry to draw
@@ -49,6 +58,36 @@ public class PointGeometry extends Geometry {
 	 */
 	public float getLongitude() {
 		return coordinates.getLongitudeE6();
+	}
+
+	@Override
+	public void draw(MapView map, Canvas c, Boolean b, Symbology s) {
+		
+		Paint paint = new Paint();
+		paint.setColor(s.getColor());
+		paint.setStyle(Style.FILL_AND_STROKE);
+		paint.setAlpha(150) ;
+
+		// Retrieving geometry and symbology
+		final int radius = s.getSize();
+
+		Point point = map.getProjection().toPixels(getCoordinates(), null);
+		// Draws the point
+		// If point is contained in the screen bounding box
+		if (c.getClipBounds().contains(point.x, point.y)) {
+			c.drawCircle(point.x, point.y, radius, paint);
+		}		
+	}
+
+	@Override
+	public boolean isSelected(MapView m, Rect click) {
+		Point ps = m.getProjection().toPixels(
+				getCoordinates(), null);
+		if (click.contains(ps.x, ps.y)) {
+			setSelected(true);
+			return true ;
+		}
+		return false;
 	}
 
 }
