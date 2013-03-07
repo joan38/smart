0682f0e3d04f295bfba120340d8ea8fd5f6f9133
tanios.faddoul <tanios.faddoul@1bc5c555-484e-e99a-1e14-4397b19f8d34>
2013-03-07 17:07:20 +0000

    Résolution sonar

--- trunk/Smart/src/fr/umlv/lastproject/smart/utils/SmartLogger.java
+++ trunk/Smart/src/fr/umlv/lastproject/smart/utils/SmartLogger.java
@@ -7,51 +7,53 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class SmartLogger {
-	
-	 private static SmartLogger locator = new SmartLogger();
-	 private Logger logger;
+public final class SmartLogger {
 
-	 /**
+	private static SmartLogger locator = new SmartLogger();
+	private Logger logger;
+	private static final int CACHE_SIZE = 5242880;
+
+	/**
 	     * 
 	     */
-	    private SmartLogger() {
-	    	initLogger();
-	    }
+	private SmartLogger() {
+		initLogger();
+	}
 
-	    /**
-	     * @return
-	     */
-	    public static SmartLogger getLocator(){
-	    	return locator;
-	    }
+	/**
+	 * @return
+	 */
+	public static SmartLogger getLocator() {
+		return locator;
+	}
 
-	    /**
-	     * @return
-	     */
-	    public Logger getLogger() {
-	    	return logger;
-	    }
+	/**
+	 * @return
+	 */
+	public Logger getLogger() {
+		return logger;
+	}
 
-	    /**
+	/**
 	     * 
 	     */
-	    private void initLogger() {
-	    	File f = new File(SmartConstants.LOG_PATH);
-	    	f.mkdir();
-	    	logger = Logger.getLogger("My General Logger");
-	    	FileHandler fh;
-	    	try {
-	    		// This block configure the logger with handler and formatter
-	    		fh = new FileHandler(SmartConstants.LOG_PATH+"/smart.log",5242880,1, true);
-	    		logger.addHandler(fh);
-	    		logger.setLevel(Level.ALL);
-	    		logger.setUseParentHandlers(false);
-	    		fh.setFormatter(new SimpleFormatter());
-	    	} catch (final SecurityException e) {
-	    		
-	    	} catch (final IOException e) {
-	    		
-	    	}
-	    }
+	private void initLogger() {
+		File f = new File(SmartConstants.LOG_PATH);
+		f.mkdir();
+		logger = Logger.getLogger("My General Logger");
+		FileHandler fh;
+		try {
+			// This block configure the logger with handler and formatter
+			fh = new FileHandler(SmartConstants.LOG_PATH + "/smart.log",
+					CACHE_SIZE, 1, true);
+			logger.addHandler(fh);
+			logger.setLevel(Level.ALL);
+			logger.setUseParentHandlers(false);
+			fh.setFormatter(new SimpleFormatter());
+		} catch (final SecurityException e) {
+
+		} catch (final IOException e) {
+
+		}
+	}
 }
